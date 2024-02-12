af5b5a968d5e10428d03416956293c3689389507
perugi <dominik.perusko@gmail.com>
2024-02-12 08:19:27 +0100

    Add jsdocs for Set

--- HashSet.js
+++ HashSet.js
@@ -1,6 +1,11 @@
 const HashMap = require('./HashMap');
 
 class HashSet extends HashMap {
+  /**
+   * Sets a value in the Set.
+   *
+   * @param {string} key - the key to set
+   */
   set(key) {
     const bucket = this._getBucket(this._hash(key));
     const listIndex = bucket.search((node) => node.value[0] === key);
@@ -14,6 +19,12 @@ class HashSet extends HashMap {
     }
   }
 
+  /**
+   * Retrieves a value from the Set based on the provided key.
+   *
+   * @param {string} key - The key to search for in the hash table
+   * @return {string|null} - The searched key, or null if the key is not found
+   */
   get(key) {
     const bucket = this._getBucket(this._hash(key));
     const listIndex = bucket.search((node) => node.value[0] === key);
